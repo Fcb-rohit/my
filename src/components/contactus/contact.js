3e92b030781e519f60ddbea1c6bf69afebe42a4f
Rohit Kashyap <33980363+Fcb-rohit@users.noreply.github.com>
2020-12-06 12:58:55 +0530

    Add files

--- src/components/contactus/contact.js
+++ src/components/contactus/contact.js
@@ -19,39 +19,8 @@ class Contact extends React.Component {
                 </div>
                 <div className="row">
                     <div className="eight columns">
-                        {/* form */}
-                        <form action="" method="post" id="contactForm" name="contactForm">
-                        <fieldset>
-                            <div>
-                                <label for="contactName">Name <span className="required">*</span></label>
-                                <input type="text" defaultValue size={35} id="contactName" name="contactName" />
-                            </div>
-                            <div>
-                                <label for="contactEmail">Email <span className="required">*</span></label>
-                                <input type="text" defaultValue size={35} id="contactEmail" name="contactEmail" />
-                            </div>
-                            <div>
-                                <label for="contactSubject">Subject</label>
-                                <input type="text" defaultValue size={35} id="contactSubject" name="contactSubject" />
-                            </div>
-                            <div>
-                                <label for="contactMessage">Message <span className="required">*</span></label>
-                                <textarea cols="50" rows="15" id="contactMessage" name="contactMessage" defaultValue={""}/>
-                            </div>
-                            <div>
-                                <button className="submit">Submit</button>
-                                <span id="image-loader">
-                                    <img alt="" src="images/loader.gif" />
-                                </span>
-                            </div>
-                        </fieldset>
-                        </form> {/* Form End */}
-                        {/* contact-warning */}
-                        <div id="message-warning"> Error boy</div>
-                        {/* contact-success */}
-                        <div id="message-success">
-                        <i className="fa fa-check" />Your message was sent, thank you!<br/>
-                        </div>
+
+
                     </div>
                     <aside className="four columns footer-widgets">
                         <div className="widget widget_contact">
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,31 +6,31 @@ class Header extends React.Component {
         return (
             <header id="home">
                 <nav id="nav-wrap">
-                    <a class="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-                    <a class="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
-                    <ul id="nav" class="nav">
-                        <li class="current"><a class="smoothscroll" href="#home">Home</a></li>
-                        <li><a class="smoothscroll" href="#about">About</a></li>
-                        <li><a class="smoothscroll" href="#resume">Resume</a></li>
-                        <li><a class="smoothscroll" href="#portfolio">Works</a></li>
-                        <li><a class="smoothscroll" href="#testimonials">Testimonials</a></li>
-                        <li><a class="smoothscroll" href="#contact">Contact</a></li>
+                    <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+                    <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+                    <ul id="nav" className="nav">
+                        <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+                        <li><a className="smoothscroll" href="#about">About</a></li>
+                        <li><a className="smoothscroll" href="#resume">Resume</a></li>
+                        <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+                        <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
+                        <li><a className="smoothscroll" href="#contact">Contact</a></li>
                     </ul>
                 </nav>>
-                <div class="row banner">
-                    <div class="banner-text">
-                        <h1 class="responsive-headline">I'm {resumeData.name}.</h1>
-                        <h3>{resumeData.intro} Let's <a class="smoothscroll" href="#about">start scrolling</a> and learn more <a class="smoothscroll" href="#about">about me</a>.</h3>
+                <div className="row banner">
+                    <div className="banner-text">
+                        <h1 className="responsive-headline">I'm {resumeData.name}.</h1>
+                        <h3>{resumeData.intro} Let's <a className="smoothscroll" href="#about">start scrolling</a> and learn more <a className="smoothscroll" href="#about">about me</a>.</h3>
                         <hr />
-                        <ul class="social">
-                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                        <ul className="social">
+                            <li><a href="#"><i className="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i className="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i className="fa fa-instagram"></i></a></li>
                         </ul>
                     </div>
                 </div>
-                <p class="scrolldown">
-                    <a class="smoothscroll" href="#about"><i class="icon-down-circle"></i></a>
+                <p className="scrolldown">
+                    <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
                 </p>
             </header>
         )
