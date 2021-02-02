6499c8c4118ce409fb89976f3b576789600a1a27
Rohit Kashyap <33980363+Fcb-rohit@users.noreply.github.com>
2021-02-02 13:23:35 +0530

    New changes

--- src/webData/data.js
+++ src/webData/data.js
@@ -2,7 +2,7 @@ let resumeData = {
     "imagebaseurl":"http://localhost:3000/",
     "name": "Rohit Kashyap",
     "intro": "I m Delhi based Web Developer",
-    "about": "As I said I'm a Web Developer, who worked on both front and backend of website,Like creating UI for frontend and database structure.I worked on Wordpress, Core Php and app framework Ionic.I have a expeirence of 2 year in working of PHP and wordpress and 1 year experience in Ionic.I created and worked on some websites development.Like pagallyrics.com,unenterprises.in,naukriconnect.com(AngluarJs)."
+    "about": "As I said I'm a Web Developer, who worked on both front and backend of website,Like creating UI for frontend and database structure.I worked on Wordpress, Core Php and app framework Ionic.I have 2.5 working expeirence in PHP and wordpress and 1 year experience in Ionic.I created and worked on some websites development.Like pagallyrics.com,unenterprises.in,naukriconnect.com(AngluarJs)."
 }
 
 export default resumeData
