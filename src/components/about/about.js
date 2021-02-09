bd62b10ca9bd86a160360e8b5d8fd3d2a051dc84
Rohit Kashyap <33980363+Fcb-rohit@users.noreply.github.com>
2021-02-09 09:52:40 +0530

    m

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -19,7 +19,6 @@ class About extends React.Component {
                                 <p className="address">
                                     <span>Rohit Kashyap</span><br />
                                     <span>New Delhi, India</span><br />
-                                    <span>(+91) 8700 1712 80</span><br />
                                     <span>rightrohit96@gmail.com</span>
                                 </p>
                             </div>
