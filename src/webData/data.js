75d12ecdf51f60854813b26b5e383f5cbac5dd17
fcb-rohit <rightrohit96@gmail.com>
2022-05-17 23:00:35 +0530

    changes

--- src/webData/data.js
+++ src/webData/data.js
@@ -2,7 +2,7 @@ let resumeData = {
     "imagebaseurl":"http://localhost:3000/",
     "name": "Rohit Kashyap",
     "intro": "I m Delhi based Web Developer",
-    "about": "I'm a Web Developer, who worked can work on backend and also customize  website,Like creating Admin dashboard using already available templates and can create whole database structure.I worked on Wordpress, Core Php and app framework Ionic. I have 4 years working expeirence in PHP and wordpress and 1 year experience in Ionic,Angular.I created and worked on some websites development.Like pagallyrics.com,unenterprises.in,naukriconnect.com(AngluarJs).I also have good understanding of Linux comman line,for example cronjobs,bash scripts, ubuntu server setup, and LAMP installation on ubuntu server.AWS is the thing which I learn and do work on this,I can create AWS instances and can work on S3 and RDS."
+    "about": "I'm a Web Developer, who worked can work on backend and also customize  website,Like creating Admin dashboard using already available templates and can create whole database structure.I worked on Wordpress, Core Php and app framework Ionic. I have 4 years working expeirence in PHP and wordpress and 1 year experience in Ionic,Angular.I created and worked on some websites development.Like pagallyrics.com,unenterprises.in,naukriconnect.com(AngluarJs).I also have good understanding of Linux comman line,for example cronjobs,bash scripts, ubuntu server setup, and LAMP installation on ubuntu server.AWS is the thing which I learn and do work on this,I can create AWS instances and can work on S3,CodeCommit and RDS."
 }
 
 export default resumeData
